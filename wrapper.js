4c72e327508554ab3fbd5a771a95799e63817789
Will Hilton <wmhilton@gmail.com>
2018-01-09 23:36:17 -0500

    feat: Throw inside a ShadowDOM so page CSS styles don't mess it up

--- wrapper.js
+++ wrapper.js
@@ -1,7 +1,12 @@
 import { ObjectInspector } from 'react-inspector'
+import retargetEvents from 'react-shadow-dom-retarget-events'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
 module.exports = (element, data) => {
-  ReactDOM.render(<ObjectInspector data={data} />, element)
+  const shadowRoot = element.attachShadow({ mode: 'open' })
+  const mountPoint = document.createElement('span')
+  shadowRoot.appendChild(mountPoint)
+  ReactDOM.render(<ObjectInspector data={data} />, mountPoint)
+  retargetEvents(shadowRoot)
 }
